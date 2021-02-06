00833fd7c7ec91928fb154055003d13bee803ef7
Shashank Shekhar <ssr3151@gmail.com>
2021-02-06 23:48:37 +0530

    database connectivity Started. Structure in progress.

--- src/main/java/com/lucipurr/tax/abstractions/ITaxService.java
+++ src/main/java/com/lucipurr/tax/abstractions/ITaxService.java
@@ -1,7 +1,10 @@
 package com.lucipurr.tax.abstractions;
 
+import com.lucipurr.tax.database.model.EmployeeInfoMaster;
 import com.lucipurr.tax.model.Employee;
 
 public interface ITaxService {
     String netTax(Employee employee);
+
+    String saveData(EmployeeInfoMaster employee);
 }
