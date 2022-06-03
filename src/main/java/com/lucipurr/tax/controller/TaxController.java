8328db592f021a4cf6ff9c9ede1959fe316cdf28
Shashank Shekhar <ssr3151@gmail.com>
2022-06-03 09:54:18 +0530

    Kafka Integration successful.

--- src/main/java/com/lucipurr/tax/controller/TaxController.java
+++ src/main/java/com/lucipurr/tax/controller/TaxController.java
@@ -1,13 +1,15 @@
 package com.lucipurr.tax.controller;
 
 import com.lucipurr.tax.abstractions.ITaxService;
-import com.lucipurr.tax.kafka.KafkaPublisher;
 import com.lucipurr.tax.model.ClientResponseVO;
 import com.lucipurr.tax.model.Response;
+import com.lucipurr.tax.service.KafkaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequestMapping("/tax")
 @Slf4j
@@ -16,9 +18,10 @@ import org.springframework.web.bind.annotation.*;
 public class TaxController {
     @Autowired
     ITaxService iTaxService;
-
     @Autowired
-    KafkaPublisher kafkaPublisher;
+    KafkaService kafkaService;
+    @Autowired
+    HttpServletRequest httpServletRequest;
 
     @GetMapping(value = "/calculate/{empId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ClientResponseVO<Response> saveData(@PathVariable String empId) {
@@ -30,8 +33,13 @@ public class TaxController {
     }
 
     @GetMapping(value = "/producer")
-    public String producer(@RequestParam("message") String message) {
-        kafkaPublisher.send(message);
+    public String producer(@RequestParam("message") String message,
+                           @RequestParam(value = "partition", required = false) String partition) {
+        if (httpServletRequest.getParameter("partition") == null) {
+            kafkaService.sendMessage(message, -1);
+        } else {
+            kafkaService.sendMessage(message, Integer.parseInt(partition));
+        }
         return "Message sent to the Kafka Topic java_in_use_topic Successfully";
     }
 }
